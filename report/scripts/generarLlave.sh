84d1c5779ea1ada1999649b171ccff6c3841aefd
Willy Villalobos <gopwivill@gmail.com>
2024-11-12 00:30:40 -0600

    Progress with written report.

--- report/scripts/generarLlave.sh
+++
@@ -1,9 +0,0 @@
-if [ -f $HOME/.ssh/authorized_keys ]
-then
-        echo "Bienvenido"
-else
-        ssh-keygen
-        cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-        cd ~/.ssh/
-        chmod 600 authorized_keys
-fi
--- report/scripts/intel_vars.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /home
-for h in * ; do
-	echo "source /opt/intel/bin/compilervars.sh intel64" >> /home/${h}/.bash_profile
-done
